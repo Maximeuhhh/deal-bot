8729271f02db566f721a413e450b6fe7a3cfbc52
Maximeuhhh <gilles.roger27@gmail.com>
2026-04-29 23:22:36 +0200

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,7 +1,2 @@
-MIN_DISCOUNT_PERCENT = 10
-MIN_TEMPERATURE = 10
-
 def is_good_deal(deal):
-    discount = deal.get("discount_percent", 0)
-    temperature = deal.get("temperature", 0)
-    return discount >= MIN_DISCOUNT_PERCENT or temperature >= MIN_TEMPERATURE
+    return True
